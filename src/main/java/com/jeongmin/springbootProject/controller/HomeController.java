9c3038019a198585d640039c39cfc021b68ba5f3
cleanert <dlawjdals990218@naver.com>
2022-05-06 04:32:32 +0900

    레이아웃 연습

--- src/main/java/com/jeongmin/springbootProject/controller/HomeController.java
+++ src/main/java/com/jeongmin/springbootProject/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping
-    public String indexTest2() {
-        return "indexTest2";
+    public String indexTest() {
+        return "indexTest";
     }
 }
 
