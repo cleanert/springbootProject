f2a7387495eabd7b02273543f31320da81b2f0d8
cleanert <dlawjdals990218@naver.com>
2022-05-08 01:04:27 +0900

    레이아웃 연습

--- src/main/java/com/jeongmin/springbootProject/controller/HomeController.java
+++ src/main/java/com/jeongmin/springbootProject/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping
-    public String indexTest() {
-        return "indexTest";
+    public String indexTestt() {
+        return "indexTestt";
     }
 }
 
---
+++ src/main/java/com/jeongmin/springbootProject/model/Board.java
@@ -0,0 +1,8 @@
+package com.jeongmin.springbootProject.model;
+
+public class Board {
+    private long id;
+    private String memberId;
+    private String title;
+    private String content;
+}
