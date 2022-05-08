61b2826c554905869651275f4e53561f657a09c2
cleanert <dlawjdals990218@naver.com>
2022-05-08 13:56:08 +0900

    레이아웃 연습

---
+++ src/main/java/com/jeongmin/springbootProject/config/MvcConfiguration.java
@@ -0,0 +1,19 @@
+package com.jeongmin.springbootProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // TODO [resources >> static >> 리소스 연결]
+        //WebMvcConfigurer.super.addResourceHandlers(registry);
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/");
+    }
+}
+
+
+
